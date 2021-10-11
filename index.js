b11fc08a17a94704866466c809b3bcb6c637fb92
Serhii Kondratiuk <sergey932015@gmail.com>
2021-10-12 00:54:30 +0300

    done

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-class CountdownTimer {}
+class CountdownTimer {
+ constructor({ targetDate, selector }) {
+  this.selectorRef = document.querySelector(selector);
+  this.daysRef = this.selectorRef.querySelector('[data-value="days"]');
+  this.hoursRef = this.selectorRef.querySelector('[data-value="hours"]');
+  this.minsRef = this.selectorRef.querySelector('[data-value="mins"]');
+  this.secsRef = this.selectorRef.querySelector('[data-value="secs"]');
+  this.targetDate = targetDate;
+  this.intervalID = null;
+  this.deltaTime = 0;
 
-const currentDate = Date.now();
-const time = targetDate - currentDate;
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+  this.start();
+ }
+ start() {
+  this.intervalID = setInterval(() => {
+   let currentTime = Date.now();
+   this.deltaTime = this.targetDate - currentTime;
 
-new CountdownTimer({
+   const days = this.pad(Math.floor(this.deltaTime / (1000 * 60 * 60 * 24)));
+   const hours = this.pad(
+    Math.floor((this.deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+   );
+   const mins = this.pad(
+    Math.floor((this.deltaTime % (1000 * 60 * 60)) / (1000 * 60)),
+   );
+   const secs = this.pad(Math.floor((this.deltaTime % (1000 * 60)) / 1000));
+   this.insertValues(days, hours, mins, secs);
+  }, 1000);
+ }
+
+ pad(value) {
+  return String(value).padStart(2, '0');
+ }
+
+ insertValues(days, hours, mins, secs) {
+  this.daysRef.textContent = days;
+  this.hoursRef.textContent = hours;
+  this.minsRef.textContent = mins;
+  this.secsRef.textContent = secs;
+ }
+}
+const timer = new CountdownTimer({
  selector: '#timer-1',
- targetDate: new Date('Jul 17, 2019'),
+ targetDate: new Date('Feb 13, 2022'),
 });
